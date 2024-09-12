abab606f9eb0ab9146ef577a7cf9d182a232468f
Olusegun <ayosegun2001@gmail.com>
2024-09-12 23:45:54 +0100

    0x03. User authentication service: Back-end - Authentification

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -118,25 +118,22 @@ def reset_password():
         return jsonify({"message": str(e)}), 403
 
 
-@app.route("/reset_password", methods=["PUT"])
-def update_password():
-    """Updates a user's password using a reset token."""
-    email = request.form.get("email")
-    reset_token = request.form.get("reset_token")
-    new_password = request.form.get("new_password")
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    """_summary_
+    Updates a user's password using a reset token
 
-
-    # Check for missing fields
-    if not all([email, reset_token, new_password]):
-        return abort(400, description="Missing fields in request")
-
+    Returns:
+        str: _description_
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
     try:
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
     except Exception:
         abort(403)
-    # except ValueError as e:
-    #     return jsonify({"message": str(e)}), 403
 
 
 if __name__ == "__main__":
