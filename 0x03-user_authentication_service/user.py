454f8d0aaa137440e6ff9eee35d4c6d343b38542
Olusegun <ayosegun2001@gmail.com>
2024-09-12 21:45:50 +0100

    0x03. User authentication service: Back-end - Authentification

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,6 +4,8 @@ User model using SQLAlchemy
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from typing import Optional
+
 
 # Create a base class using declarative_base()
 Base = declarative_base()
@@ -16,8 +18,16 @@ class User(Base):
     __tablename__ = 'users'
 
     # Define the columns for the users table
-    id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    id: int = Column(Integer, primary_key=True)
+    email: str = Column(String(250), nullable=False)
+    hashed_password: str = Column(String(250), nullable=False)
+    session_id: Optional[str] = Column(String(250), nullable=True)
+    reset_token: Optional[str] = Column(String(250), nullable=True)
+
+    def __init__(self, email: str, hashed_password: str,
+                 session_id: Optional[str] = None,
+                 reset_token: Optional[str] = None):
+        self.email = email
+        self.hashed_password = hashed_password
+        self.session_id = session_id
+        self.reset_token = reset_token
